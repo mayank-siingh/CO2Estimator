2718ffd698e803ab793f535dd3a7d88f57826d01
Mayank Singh <51783955+mayank-siingh@users.noreply.github.com>
2020-03-20 20:49:47 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,9 +7,8 @@ setup(
     license='MIT license',
     url = 'https://github.com/mayank-siingh/CO2Estimator/',
     author = 'Mayank Bhandari',
-	author_email = 'bhandarimayank01@gmail.com',
-    download_url ='https://github.com/mayank-siingh/CO2Estimator/archive/v0.1.0.tar.gz',
-)
+    author_email = 'bhandarimayank01@gmail.com'
+    )
 
 
 
