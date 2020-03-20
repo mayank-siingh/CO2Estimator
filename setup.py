d25bce806e76f8164917ec63ee5ee6b3cc9bb904
Mayank Singh <51783955+mayank-siingh@users.noreply.github.com>
2020-03-20 15:26:31 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='co2Estimator',
     version='0.1.0',
-    packages=['co2_estimator',],
+    packages=['co2Estimator',],
     license='MIT license',
     url = 'https://github.com/mayank-siingh/CO2Estimator/',
     author = 'Mayank Bhandari'
