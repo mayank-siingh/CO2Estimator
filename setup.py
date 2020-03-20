771a086f3de1e9ddcafda77c0213c622e4b5f7c6
Mayank Singh <51783955+mayank-siingh@users.noreply.github.com>
2020-03-20 15:18:07 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from distutils.core import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
 setup(
     name='co2Estimator',
-    version='1.0',
+    version='0.1.0',
     packages=['co2_estimator',],
-    license='MIT license',
-    long_description=open('README.txt').read()
+    license='MIT license'
 )
