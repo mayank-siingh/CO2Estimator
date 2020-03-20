2a69e8c81e2a069675314c9b4daf911e1a5b6458
Mayank Singh <51783955+mayank-siingh@users.noreply.github.com>
2020-03-20 15:09:57 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='co2Estimator',
-    version='1.0dev',
+    version='1.0',
     packages=['co2_estimator',],
     license='MIT license',
     long_description=open('README.txt').read()
