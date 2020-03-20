76f880c0cbb8e720546ecb67dbd4a2d23a03bb60
Mayank Singh <51783955+mayank-siingh@users.noreply.github.com>
2020-03-20 14:50:32 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='co2Estimator',
+    version='1.0dev',
+    packages=['co2_estimator',],
+    license='MIT license',
+    long_description=open('README.txt').read()
+)
